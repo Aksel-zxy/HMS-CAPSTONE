6c72219d7193b6d8f8bedb2bbaca383a523ecb23
Aksel-zxy <184739488+Aksel-zxy@users.noreply.github.com>
2025-08-01 13:01:28 +0800

    Add files via upload

---
+++ backend/Pharmacy Management/pharmacy_med inventory.php
@@ -0,0 +1,278 @@
+<?php
+include '../../SQL/config.php';
+
+if (!isset($_SESSION['pharmacy']) || $_SESSION['pharmacy'] !== true) {
+    header('Location: login.php'); // Redirect to login if not logged in
+    exit();
+}
+
+if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
+    echo "User ID is not set in session.";
+    exit();
+}
+
+$query = "SELECT * FROM users WHERE user_id = ?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("i", $_SESSION['user_id']);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+
+if (!$user) {
+    echo "No user found.";
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HMS | Pharmacy Management</title>
+    <link rel="shortcut icon" href="assets/image/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="assets/CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/CSS/super.css">
+    <link rel="stylesheet" href="assets/CSS/med_inventory.css">
+</head>
+
+<body>
+    <div class="d-flex">
+        <!----- Sidebar ----->
+        <aside id="sidebar" class="sidebar-toggle">
+
+            <div class="sidebar-logo mt-3">
+                <img src="assets/image/logo-dark.png" width="90px" height="20px">
+            </div>
+
+            <div class="menu-title">Pharmacy Management | Medicine Inventory</div>
+
+            <!----- Sidebar Navigation ----->
+        
+            <li class="sidebar-item">
+                <a href="pharmacy_dashboard.php" class="sidebar-link" data-bs-toggle="#" data-bs-target="#"
+                    aria-expanded="false" aria-controls="auth">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cast" viewBox="0 0 16 16">
+                        <path d="m7.646 9.354-3.792 3.792a.5.5 0 0 0 .353.854h7.586a.5.5 0 0 0 .354-.854L8.354 9.354a.5.5 0 0 0-.708 0" />
+                        <path d="M11.414 11H14.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-13a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .5.5h3.086l-1 1H1.5A1.5 1.5 0 0 1 0 10.5v-7A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v7a1.5 1.5 0 0 1-1.5 1.5h-2.086z" />
+                    </svg>
+                    <span style="font-size: 18px;">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a href="pharmacy_med inventory.php" class="sidebar-link" data-bs-toggle="#" data-bs-target="#"
+                    aria-expanded="false" aria-controls="auth">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="fa-solid fa-capsules" viewBox="0 0 16 16">
+                        <path d="m7.646 9.354-3.792 3.792a.5.5 0 0 0 .353.854h7.586a.5.5 0 0 0 .354-.854L8.354 9.354a.5.5 0 0 0-.708 0" />
+                        <path d="M11.414 11H14.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-13a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .5.5h3.086l-1 1H1.5A1.5 1.5 0 0 1 0 10.5v-7A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v7a1.5 1.5 0 0 1-1.5 1.5h-2.086z" />
+                    </svg>
+                    <span style="font-size: 18px;">Medicine Inventory</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a href="pharmacy_sales.php" class="sidebar-link" data-bs-toggle="#" data-bs-target="#"
+                    aria-expanded="false" aria-controls="auth">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="fa-solid fa-chart-line" viewBox="0 0 16 16">
+                        <path d="m7.646 9.354-3.792 3.792a.5.5 0 0 0 .353.854h7.586a.5.5 0 0 0 .354-.854L8.354 9.354a.5.5 0 0 0-.708 0" />
+                        <path d="M11.414 11H14.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-13a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .5.5h3.086l-1 1H1.5A1.5 1.5 0 0 1 0 10.5v-7A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v7a1.5 1.5 0 0 1-1.5 1.5h-2.086z" />
+                    </svg>
+                    <span style="font-size: 18px;">Sales</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a href="pharmacy_prescription.php" class="sidebar-link" data-bs-toggle="#" data-bs-target="#"
+                    aria-expanded="false" aria-controls="auth">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="fa-solid fa-file-prescription" viewBox="0 0 16 16">
+                        <path d="m7.646 9.354-3.792 3.792a.5.5 0 0 0 .353.854h7.586a.5.5 0 0 0 .354-.854L8.354 9.354a.5.5 0 0 0-.708 0" />
+                        <path d="M11.414 11H14.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-13a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .5.5h3.086l-1 1H1.5A1.5 1.5 0 0 1 0 10.5v-7A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v7a1.5 1.5 0 0 1-1.5 1.5h-2.086z" />
+                    </svg>
+                    <span style="font-size: 18px;">Prescription</span>
+                </a>
+            </li>
+
+            
+        </aside>
+        <!----- End of Sidebar ----->
+        <!----- Main Content ----->
+        <div class="main">
+            <div class="topbar">
+                <div class="toggle">
+                    <button class="toggler-btn" type="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="currentColor" class="bi bi-list-ul"
+                            viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="logo">
+                    <div class="dropdown d-flex align-items-center">
+                        <span class="username ml-1 me-2"><?php echo $user['fname']; ?> <?php echo $user['lname']; ?></span><!-- Display the logged-in user's name -->
+                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi bi-person-circle"></i>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="min-width: 200px; padding: 10px; border-radius: 5px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); background-color: #fff; color: #333;">
+                            <li style="margin-bottom: 8px; font-size: 14px; color: #555;">
+                                <span>Welcome <strong style="color: #007bff;"><?php echo $user['lname']; ?></strong>!</span>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="../logout.php" style="font-size: 14px; color: #007bff; text-decoration: none; padding: 8px 12px; border-radius: 4px; transition: background-color 0.3s ease;">
+                                    Logout
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+            <!-- START CODING HERE -->
+      <main class="main-content">
+      <div class="top-bar">
+        <span class="user-name"></span>
+      </div>
+
+      <div class="dashboard-content">
+        <h2>Medicine Inventory</h2>
+            <button class="add-btn" onclick="openModal()">+ Add Medicine</button>
+
+        <table class="inventory-table" id="medicineTable">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Medicine Name</th>
+              <th>Brand</th>
+              <th>Description</th>
+              <th>Category</th>
+              <th>Stock</th>
+              <th>Price</th>
+              <th>Expiry Date</th>
+              <th>Date Added</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+             <td class="row-number"></td>
+              <td>Yakapsul</td>
+              <td>Tablet</td>
+              <td>Yakap nya</td>
+              <td>Tablet</td>
+              <td>150</td>
+               <td><span>&#8369;</span> 10.50</td>
+              <td>2026-01-20</td>
+              <td class="date-added">Auto</td>
+              <td>
+                <button class="edit-btn" onclick="editRow(this)">Edit</button>
+                <button class="delete-btn" onclick="deleteRow(this)">Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td class="row-number"></td>
+              <td>Amoxicillin</td>
+              <td>Capsule</td>
+              <td>dama mo</td>
+              <td>Capsule</td>
+              <td>90</td>
+               <td><span>&#8369;</span> 8.50</td>
+              <td>2025-10-10</td>
+              <td class="date-added">Auto</td>
+              <td>
+                <button class="edit-btn" onclick="editRow(this)">Edit</button>
+                <button class="delete-btn" onclick="deleteRow(this)">Delete</button>
+              </td>
+            </tr>
+              <tr>
+              <td class="row-number"></td>
+              <td>Kisspirin</td>
+              <td>Capsule</td>
+              <td>Kiss</td>
+              <td>Capsule</td>
+              <td>90</td>
+              <td><span>&#8369;</span> 5.50</td>
+              <td>2025-28-10</td>
+              <td class="date-added">Auto</td>
+              <td>
+                <button class="edit-btn" onclick="editRow(this)">Edit</button>
+                <button class="delete-btn" onclick="deleteRow(this)">Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td class="row-number"></td>
+              <td>bading</td>
+              <td>jacob brand</td>
+              <td>is kabadingan</td>
+              <td>Capsule</td>
+              <td>2000</td>
+              <td><span>&#8369;</span> 20.00</td>
+              <td>2030-10-10</td>
+              <td class="date-added">Auto</td>
+              <td>
+                <button class="edit-btn" onclick="editRow(this)">Edit</button>
+                <button class="delete-btn" onclick="deleteRow(this)">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  </div>
+
+
+<div id="medicineModal" class="modal hidden">
+  <div class="modal-content">
+    <span class="close-btn" onclick="closeModal()">&times;</span>
+    <h3 id="modalTitle">Add Medicine</h3>
+    <form id="medicineForm" onsubmit="saveMedicine(event)">
+  <input type="hidden" id="editIndex" value="">
+
+  <label>Medicine Name:</label>
+  <input type="text" id="medName" required>
+
+  <label>Brand:</label>
+  <input type="text" id="medBrand" required>
+
+  <label>Description:</label>
+  <textarea id="medDescription" required></textarea>
+
+  <label>Category:</label>
+  <input type="text" id="medCategory" required>
+
+  <label>Stock:</label>
+  <input type="number" id="medStock" required>
+
+  <label>Price (₱):</label>
+  <input type="number" id="medPrice" step="0.01" required>
+
+  <label>Expiry Date:</label>
+  <input type="date" id="medExpiry" required>
+
+  <!-- ✅ Add this -->
+  <label>Date Added:</label>
+  <input type="date" id="medDateAdded" readonly>
+
+  <button type="submit">Save</button>
+</form>
+  </div>
+</div>
+            <!-- END CODING HERE -->
+        </div>
+        <!----- End of Main Content ----->
+    </div>
+    <script>
+        const toggler = document.querySelector(".toggler-btn");
+        toggler.addEventListener("click", function() {
+            document.querySelector("#sidebar").classList.toggle("collapsed");
+        });
+    </script>
+    <script src="assets/Bootstrap/all.min.js"></script>
+    <script src="assets/Bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="assets/Bootstrap/fontawesome.min.js"></script>
+    <script src="assets/Bootstrap/jq.js"></script>
+    <script src="assets/Bootstrap/med_inventory.js"></script>
+</body>
+
+</html>
