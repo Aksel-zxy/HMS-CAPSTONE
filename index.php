a5ae37052f9e46f2e6e94d59286437c32778f3e6
geraldcave <g.cabelin231@gmail.com>
2025-09-04 20:43:58 +0800

    hahaha

--- index.php
+++ index.php
@@ -26,7 +26,6 @@
                     <ul class="nav-menu">
                         <li class="menu-active"><a href="index.php">Home</a></li>
                         <li><a href="backend/login.php">Login</a></li>
-                    <li><a href="backend/join_our_team.php">Join Our Team</a></li>
                         <li><a href="backend/join_our_team.php">Join Our Team</a></li>
                     </ul>
                 </nav>	    		
