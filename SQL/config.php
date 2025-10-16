88010c203d916497eaf00e10445d8a17780021db
Ezekielskie <deminetel04@gmail.com>
2025-10-16 19:39:09 +0800

    added main sql config to report branch

--- SQL/config.php
+++ SQL/config.php
@@ -1,57 +1,53 @@
-<<<<<<< HEAD
-    <?php
+<?php
+if (!defined("BASE_URL")) {
+    define("BASE_URL", "/hms-capstone/");
+}
 
-
-    if (!defined("BASE_URL")) {
-        define("BASE_URL", "/hms-capstone/");
-    }
-
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    $inactive = 1800;
-    if (isset($_SESSION['timeout'])) {
-        $session_life = time() - $_SESSION['timeout'];
-        if ($session_life > $inactive) {
-            session_unset();
-            session_destroy();
-            echo "<script>
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+$inactive = 1800;
+if (isset($_SESSION['timeout'])) {
+    $session_life = time() - $_SESSION['timeout'];
+    if ($session_life > $inactive) {
+        session_unset();
+        session_destroy();
+        echo "<script>
             alert('You have been logged out due to inactivity.');
             window.location.href = '" . BASE_URL . "backend/logout.php';
         </script>";
-            exit();
-        }
+        exit();
     }
-    $_SESSION['timeout'] = time();
+}
+$_SESSION['timeout'] = time();
 
 
-    // MySQL settings
+// MySQL settings
 
-    $host     = "127.0.0.1";
-    $port     = "3306";
-    $dbname   = "hmscapstone";
-    $username = "root";
-    $password = "";
-
-
-    // mysqli connection
-
-    $conn = new mysqli($host, $username, $password, $dbname, $port);
-    if ($conn->connect_error) {
-        die("Connection failed (MySQLi): " . $conn->connect_error);
-    }
+$host     = "127.0.0.1";
+$port     = "3306";
+$dbname   = "hmscapstone";
+$username = "root";
+$password = "";
 
 
-    // PDO connection
+// mysqli connection
 
-    try {
-        $pdo = new PDO(
-            "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
-            $username,
-            $password
-        );
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        die("Connection failed (PDO): " . $e->getMessage());
-    }
-    ?>=======>>>>>>> main
+$conn = new mysqli($host, $username, $password, $dbname, $port);
+if ($conn->connect_error) {
+    die("Connection failed (MySQLi): " . $conn->connect_error);
+}
+
+
+// PDO connection
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed (PDO): " . $e->getMessage());
+}
