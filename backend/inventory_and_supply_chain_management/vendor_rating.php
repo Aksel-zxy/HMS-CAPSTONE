88d3f4eb0e08def2a6a6414330fdd2940c22acbe
Christian <khiane01111@gmail.com>
2025-10-24 01:19:55 +0800

    modified:   backend/inventory_and_supply_chain_management/assets/CSS/vendor_documents.css 	modified:   backend/inventory_and_supply_chain_management/assets/CSS/vendor_orders.css 	modified:   backend/inventory_and_supply_chain_management/assets/CSS/vendor_products.css 	modified:   backend/inventory_and_supply_chain_management/assets/CSS/vendor_registraion.css 	modified:   backend/inventory_and_supply_chain_management/assets/CSS/vendorcontract.css 	modified:   backend/inventory_and_supply_chain_management/assets/CSS/vendorsidebar.css 	modified:   backend/inventory_and_supply_chain_management/vendor_rating.php

--- backend/inventory_and_supply_chain_management/vendor_rating.php
+++ backend/inventory_and_supply_chain_management/vendor_rating.php
@@ -58,7 +58,7 @@ function displayStars($avg) {
 <title>Vendor Ratings & Feedback</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-<link rel="stylesheet" href="assets/css/Inventory_dashboard.css">
+<link rel="stylesheet" href="assets/css/inventory_dashboard.css">
 </head>
 <body class="bg-light">
 
