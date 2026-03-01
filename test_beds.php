d8a66f152d66f01edd2a4192edaf51decae46b84
geraldcave <g.cabelin231@gmail.com>
2026-03-01 22:50:33 +0800

    doners?

--- test_beds.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'C:/xampp/htdocs/HMS-CAPSTONE/SQL/config.php';
-$result = $conn->query("DESCRIBE p_beds");
-while ($row = $result->fetch_assoc()) {
-    echo $row['Field'] . " - " . $row['Type'] . "\n";
-}
-?>
